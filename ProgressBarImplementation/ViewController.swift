5e1b208b087a1f26dcff67fcf0817ed83c514160
Sugirdha <sugirdha.v@gmail.com>
2023-10-01 16:22:20 +0800

    Initial Commit

---
+++ ProgressBarImplementation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgressBarImplementation
+//
+//  Created by Sugirdha Vaithyanathan on 1/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
