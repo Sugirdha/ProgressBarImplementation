3ed2ad69b2f391c3f7a10c16a5f4921298f1781b
Sugirdha <sugirdha.v@gmail.com>
2023-10-24 12:36:18 +0800

    working progressbar implementation for displaying progress bar in subsequent screens

--- ProgressBarImplementation/HomeViewController.swift
+++ ProgressBarImplementation/HomeViewController.swift
@@ -7,15 +7,18 @@
 
 import UIKit
 
-class HomeViewController: BaseViewController {
-    let nextButton: UIButton = {
+class HomeViewController: UIViewController {
+    
+    var progressConstant: Float = 0.0
+    
+    let showProgressButton: UIButton = {
         let btn = UIButton(type: .system)
         btn.backgroundColor = .black
         btn.tintColor = .white
-        btn.setTitle("Next", for: .normal)
+        btn.setTitle("Show ProgressBar", for: .normal)
         return btn
     }()
-    
+
     let barView: UIView = {
         let view = UIView()
         return view
@@ -27,70 +30,35 @@ class HomeViewController: BaseViewController {
         layout()
     }
 
-    private func layout() {
+    func layout() {
         barView.translatesAutoresizingMaskIntoConstraints = false
-        
-        nextButton.translatesAutoresizingMaskIntoConstraints = false
-        nextButton.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
 
-        [barView, nextButton].forEach(view.addSubview(_:))
-        
+        showProgressButton.translatesAutoresizingMaskIntoConstraints = false
+        showProgressButton.addTarget(self, action: #selector(showProgressButtonTapped), for: .touchUpInside)
+
+        [barView, showProgressButton].forEach(view.addSubview(_:))
+
         NSLayoutConstraint.activate([
-            barView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            barView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            barView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             barView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             barView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             barView.heightAnchor.constraint(equalToConstant: 100),
 
-            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            nextButton.heightAnchor.constraint(equalToConstant: 24),
-            nextButton.widthAnchor.constraint(equalToConstant: 80),
+            showProgressButton.heightAnchor.constraint(equalToConstant: 24),
+            showProgressButton.topAnchor.constraint(equalTo: barView.bottomAnchor, constant: 48),
+            showProgressButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            showProgressButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
         ])
-        
-        displayProgressBar(in: barView, progress: progressConstant)
     }
     
-    @objc func nextButtonTapped() {
-        progressConstant += 5
-        
-    }
+    @objc func showProgressButtonTapped() {
+        barView.displayProgressBar(progress: progressConstant)
+        if progressConstant >= 1.0 {
+            showProgressButton.isEnabled = false
+        }
+        progressConstant += 0.1
+        showProgressButton.setTitle("Update Progress", for: .normal)
+        }
 
-}
-
-// MARK: - BaseViewController
-class BaseViewController: UIViewController {
-    var progressConstant = 5
-
-}
-
-// MARK: - ProgressBar
-extension BaseViewController {
-    func displayProgressBar(in parentView: UIView, progress: Int) {
-        let progressBar = ProgressBarView(frame: parentView.frame, progress: progress)
-        view.addSubview(progressBar)
-        progressBar.topAnchor.constraint(equalTo: parentView.bottomAnchor, constant: 10).isActive = true
-        progressBar.leadingAnchor.constraint(equalTo: parentView.leadingAnchor).isActive = true
-        progressBar.trailingAnchor.constraint(equalTo: parentView.trailingAnchor).isActive = true
-        progressBar.heightAnchor.constraint(equalToConstant: 6).isActive = true
-    }
-}
-
-// MARK: - Other VCs Placeholders
-class OneViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemCyan
-    }
-}
-class TwoViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemPink
-    }
-}
-class ThreeViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemMint
-    }
 }
